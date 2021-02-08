56a6ecae41e213c4d7b8435fa5780a8e57dc7bc9
Kanat Karimov <Karash1985@gmail.com>
2021-02-08 18:20:32 +0600

    classes accessor methods

---
+++ HomeWork_09_12_2020/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWork_09_12_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // тип Имя = new Конструктор (параметры);
+            Rocket rocket = new Rocket("реактивный", 90, 1800, "жидкий метан", "космический");
+            Console.WriteLine();
+            Rocket.diameter++;
+            Console.WriteLine(Rocket.diameter);
+            Rocket.netWeight++;
+            Console.WriteLine(Rocket.netWeight);
+
+            var RocketDiameter = Rocket.GetRocketDiametr();
+
+            //Rocket[] rockets = { new Rocket(), new Rocket() };
+            //Array.Resize(ref rokets, 5);
+
+
+        }
+    }
+    
+    public partial class Roket
+    {
+        public void ParseTextToInt(string text)
+        { 
+            
+        }           
+         
+    } 
+}
---
+++ HomeWork_09_12_2020/Rocket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_09_12_2020
+{
+    public partial class Rocket
+    {
+        // Характеристики - полями
+        // модификаторы доступа \\ доп. модификаторы \\ тип имяПоля
+        private string _engine; // Двигатель
+        private int _proportions; // Габариты
+        private double _parameters; // Скорость полета
+        private string _fuelType; // Тип топлива
+        private string _rocketType; // Тип ракеты - в зависимости от назначения, космические, грузовые, военные
+        public static int diameter = 50; // диаметр ракеты
+        public static int netWeight = 150; // вес ракеты без груза
+
+
+        // Конструктор  / Мод.доступа  имяКласса(параметры) {тело;}
+        public Rocket(string engine, int proportions, double parameters, string fuelType, string rocketType)
+        {
+            _engine = engine;
+            _proportions = proportions;
+            _parameters = parameters;
+            _fuelType = fuelType;
+            _rocketType = rocketType;
+        }
+        public static int GetRocketDiametr()
+        {
+            return diameter;
+        }
+
+        // Методы доступа к полям (геттеры и сеттеры)
+        // мод. доступа возвр. тип имяМетода (параметры)
+        // { тело; returne значение (опционально); }
+        public string GetEngine(string engine)
+        {
+            return engine;
+        }
+        public void SetProportions(int proportions)
+        {
+            _proportions = proportions;
+        }
+        public void SetParameters(double parameters)
+        {
+            _parameters = parameters;
+        }
+        
+
+    }
+}
